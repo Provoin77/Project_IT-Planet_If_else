8d88c7cd9d9c867b4b18008513a8c573e5cc41e1
Provoin77 <prokhorov.98988@gmail.com>
2025-05-16 20:24:36 +0500

    Update run.py

--- run.py
+++ run.py
@@ -3,4 +3,7 @@ from app.app import app, init_db
 
 if __name__ == '__main__':
     init_db()
-    app.run(host='0.0.0.0', port=80)
+     # берём порт из окружения Render или дефолт 80 для локальной отладки
+     import os
+     port = int(os.environ.get('PORT', 80))
+     app.run(host='0.0.0.0', port=port)
