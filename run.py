827a849a53d0a460642d5495be4a30d04e86f8b9
Provoin77 <prokhorov.98988@gmail.com>
2025-05-16 21:00:59 +0500

    Update run.py

--- run.py
+++ run.py
@@ -1,9 +1,12 @@
-# run.py
 from app.app import app, init_db
 
 if __name__ == '__main__':
+    # Инициализация БД (если нужно)
     init_db()
-     # берём порт из окружения Render или дефолт 80 для локальной отладки
-     import os
-     port = int(os.environ.get('PORT', 80))
-     app.run(host='0.0.0.0', port=port)
+
+    # Получаем порт из переменной окружения (Render подставит свой)
+    import os
+    port = int(os.environ.get('PORT', 80))
+
+    # Запускаем Flask на 0.0.0.0:PORT
+    app.run(host='0.0.0.0', port=port)
